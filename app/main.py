bd411caebabd751bb46bbcbcb62a9964161c8efb
PintoJR <guimoraespinto@gmail.com>
2026-09-23 20:44:26 -0300

    Solution02

--- app/main.py
+++ app/main.py
@@ -2,14 +2,13 @@ from datetime import datetime
 import time
 
 
-def main():
+def main() -> None:
     while True:
         now = datetime.now()
-        with open("app-{now.hour}_{now.minute}_{now.second}.log", "w") as file:
-            print(now.strftime("%Y-%m-%d %H:%M:%S"))
-        time.sleep(1)    
-
-    print(datetime.now())        
+        with open(f"app-{now.hour}_{now.minute}_{now.second}.log", "w") as file:
+            file.write(now.strftime("%Y-%m-%d %H:%M:%S"))
+        time.sleep(1)
+    print(datetime.now())
 
 
 if __name__ == "__main__":
