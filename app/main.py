ee12a384c366fa660b58d5b3a26c726b97cae5dd
PintoJR <guimoraespinto@gmail.com>
2026-09-23 20:55:23 -0300

    Solution03

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ import time
 def main() -> None:
     while True:
         now = datetime.now()
-        with open(f"app-{now.hour}_{now.minute}_{now.second}.log", "w") as file:
+        with open(f"app-{now.hour}_{now.minute}_{now.second}.log",
+                   "w") as file:
             file.write(now.strftime("%Y-%m-%d %H:%M:%S"))
         time.sleep(1)
     print(datetime.now())
